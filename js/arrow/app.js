c0ba4d944a3ae80dbc72e4fa7499bab08ae8fbc8
Carlos Aranzazu <caaranzazu_230@cue.edu.co>
2024-04-15 12:30:55 -0500

    Se crea primer commit

---
+++ js/arrow/app.js
@@ -0,0 +1,13 @@
+//crear un programa que calcule si un numero es primo o no usando arrow functions
+
+const isPrime = (num) => {
+    for(let i = 2; i < num; i++)
+        if(num % i === 0) return false;
+    return num > 1;
+}
+
+console.log(isPrime(7));
+
+
+
+
